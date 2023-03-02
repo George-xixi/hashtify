e6f2778119f90b07faa5fffbbe788367a1554c8b
George Aldington <georgealdington@gmail.com>
2023-03-02 15:31:26 +0000

    creates Login component at home route

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
 import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hashtag Generator</h1>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="login">
+      <h1>#Hashtify</h1>
+      <h2>Log in to search, create and manage your hashtags.</h2>
+      <button type="button">Log in</button>
+      <h2>Or you can search without logging in.</h2>
+      <button type="button">I just want to search</button>
+    </div>
+  );
+}
+
+export default Login;
