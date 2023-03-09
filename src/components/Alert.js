bc81b94f5c27912c738b76039ba7e1dfad6bfc84
Yan Fung <fungmingyan@hotmail.com>
2023-03-09 17:29:32 +0000

    Created Alert component for rending success or error message

---
+++ src/components/Alert.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+const Alert = ({ message, success }) => {
+  if (!message) {
+    return null;
+  }
+  return (
+    <div className={`alert-${success ? "success" : "error"}`}>
+      {message === "Successfully saved" ? (
+        <FontAwesomeIcon icon={faCheck} />
+      ) : (
+        <FontAwesomeIcon icon={faTimes} />
+      )}
+      {message}
+    </div>
+  );
+};
+
+export default Alert;
+
+Alert.defaultProps = {
+  success: false,
+};
+
+Alert.propTypes = {
+  message: PropTypes.string.isRequired,
+  success: PropTypes.bool,
+};
